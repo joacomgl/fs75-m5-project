5b773a20a45f09e5fbc5de04ac763af1e615b4a6
Joaco <mgljoaco@gmail.com>
2026-08-12 19:56:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { JSX } from 'react';
+
+function App(): JSX.Element {
+  return (
+    <div style={{padding: '2rem'}}>
+      <h1>HENRY-commerce</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//* UI (botones, inputs, divs, etc)
+//* Componentes: Navbar, Sidebar, Footer, ProductCard, etc.
+//* Layout: Navbar + Sidebar + Footer, ProductList, ProductDetail, etc.
+//* Page: Ruta -> /products -> ProductPage
